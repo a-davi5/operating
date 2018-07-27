da471c1bcd7fc216889b03a4a13d0d03566fc60d
Adam Davis <adam.davis@stfc.ac.uk>
2018-07-27 08:55:48 +0100

    Commented the file so this makes sense to everyone and myself for the future

--- plotfine.py
+++ plotfine.py
@@ -6,109 +6,63 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import glob, os
 
+#author Adam Davis
 
-file_name="adc_cal_AUXSAMPLE_107.h5"
-filelist=[]
-
-def flatten3(input):
-    new_list = []
-    for i in input:
-        for j in i:
-	    for k in j:
-		#l=k^63
-		#m=(k&960)+l
-                new_list.append((k&960)+(k&63)^63)
-    return new_list
-
+#function to extract the fine data bits from the H5 file (chosen column 33 in this case)
 def getfinebitscolumn(input):
-    new_list = []
+    fine_data = []
     for i in input:
         for j in i:
-		#o=(j[33])
-		#k=(o&63)
-		#l=k^63
-		#m=((o&960)+l)
-		#new_list.append(m)
-		#new_list.append((j[33]&960)-(j[33]&63)) # subtract the last 6 bits from the coarse bits
-		new_list.append((j[33]&63)) # extract the fine bits
-		#new_list.append(j[33])
-		#new_list.append((j[33]&992)+(j[33]&31)^31) #this is the last 5 bits inverted
-		#new_list.append((j[33]&960) + (j[33]&63)^63)
-    return new_list
+		fine_data.append((j[33]&63)) # extract the fine bits
+    return fine_data
 
-def flatten2(input):
-    new_list = []
-    for i in input:
-        for j in i:
-		#o=(j[33])
-		#k=(o&63)
-		#l=k^63
-		#m=((o&960)+l)
-		#new_list.append(m)
-		new_list.append((j[33]&960)-(j[33]&63)) # subtract the last 6 bits from the coarse bits
-		#new_list.append(j[33])
-		#new_list.append((j[33]&992)+(j[33]&31)^31) #this is the last 5 bits inverted
-		#new_list.append((j[33]&960) + (j[33]&63)^63)
-    return new_list
-
+#function to list the h5 files in a specific directory
 def Listh5Files():
     filenames=[]
-    #os.chdir("/mydir")
+    #list the files in this directory
     for file in glob.glob("/scratch/qem/fine/*.h5"):
         filenames.append(file)
-        #print(file)
+    #sort files alphabetically
     filenames.sort()
     return filenames
 
-filelist=Listh5Files()
-
-#print(filelist)
-#print(len(filelist))
-
+#function to generate the voltages given a specific length
 def generatevoltages(length):
 	voltages=[]
 	for i in range(length):
 		voltages.append(float(1 + (i * 0.00008)))
-
 	return voltages
 
-
+######    MAIN    ######
+#array of files to be processed
+filelist=[]
+filelist=Listh5Files()
+# voltages for the plot
 voltages = []
 voltages = generatevoltages(len(filelist))
-
+# averaged data for the plot 
 averages = []
+
+# extract the data from each file in the folder
 for i in filelist:
-	#print(filelist[i])
+	#open the file in the filelist array
 	f=h5py.File(i, 'r')
-	#print("Keys: %s" % f.keys())
-	#f.close()
 	print(i)
+	#extract the data key from the file
 	a_group_key = list(f.keys())[0]
+	#get the data
 	data = list(f[a_group_key])
-	#plt.clf()
-
+	#get data for column
 	column=getfinebitscolumn(data)
-	#print(column)
+	#average the column data
 	average = sum(column) / len(column)
-	#print(average)
+	#add the averaged data to the averages[] array
 	averages.append(average)
-	
-
-	#for i in flat
-	
-	#plt.hist(flat, bins=5, align='mid', histtype='step')	
-	#plt.hist(flat, bins=1000, range=[0,1000], align='mid', histtype='step')  # arguments are passed to np.histogram
-	#plt.ylabel('counts')
-	#plt.xlabel('ADC value')
-	#plt.xaxis.set_major_locator(MaxNLocator(integer=true))
-	#plt.grid(True)
-	#plt.title(i)
-	#plt.savefig("temp_%s.png" %i, dpi = 100)
+	#close the file
 	f.close()
 
-#print(averages)
-#print(voltages)
 
+#generate the x / y plot of the data collected
 plt.plot(voltages, averages, '-')
 plt.grid(True)
 plt.xlabel('Voltage')
@@ -117,22 +71,3 @@ plt.savefig("fine.png", dpi = 100)
 plt.show()
 
 
-"""
-
-f=h5py.File(file_name, 'r')
-print("Keys: %s" % f.keys())
-for key in f.keys():
-    print(key) #Names of the groups in HDF5 file.
-a_group_key = list(f.keys())[0]
-
-# Get the data
-data = list(f[a_group_key])
-flat=flatten3(data)
-print(len(data))
-plt.hist(flat, bins=99, range=[0,1000], align='mid', histtype='step')  # arguments are passed to np.histogram
-plt.grid(True)
-plt.title(file_name)
-plt.savefig("%s.png" %file_name, dpi = 50)
-plt.show()
-
-"""
